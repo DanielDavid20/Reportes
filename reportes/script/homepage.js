ec5e24219d1ce5cdcebc4a00cff6eaf04f0ea3b4
DanielDavid20 <danielmoralesdoria14@gmail.com>
2025-03-27 16:17:01 -0500

    Add files via upload

---
+++ reportes/script/homepage.js
@@ -0,0 +1,91 @@
+// Importar las funciones que necesitas desde los SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut, deleteUser } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
+import { getFirestore, getDoc, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-firestore.js";
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyAXszytDgsmZyreTP9aLHBMYXno-Zj3oyQ",
+    authDomain: "login-form-ca18a.firebaseapp.com",
+    projectId: "login-form-ca18a",
+    storageBucket: "login-form-ca18a.firebasestorage.app",
+    messagingSenderId: "872700542438",
+    appId: "1:872700542438:web:5f59303405574e7194aa27"
+};
+
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore();
+
+// Observa el estado de autenticación
+onAuthStateChanged(auth, (user) => {
+    const loggedInUserId = localStorage.getItem('loggedInUserId');
+    if (loggedInUserId) {
+        console.log(user);
+        const docRef = doc(db, "users", loggedInUserId);
+        getDoc(docRef)
+            .then((docSnap) => {
+                if (docSnap.exists()) {
+                    const userData = docSnap.data();
+                    document.getElementById('loggedUserFName').innerText = userData.firstName;
+                    document.getElementById('loggedUserEmail').innerText = userData.email;
+                    document.getElementById('loggedUserLName').innerText = userData.lastName;
+
+                } else {
+                    console.log("No se encontró un documento que coincida con el ID");
+                }
+            })
+            .catch((error) => {
+                console.log("Error al obtener el documento: ", error);
+            });
+    } else {
+        console.log("User ID not found in local storage");
+    }
+});
+
+// Manejo del botón de cierre de sesión
+const logoutButton = document.getElementById('logout');
+
+logoutButton.addEventListener('click', () => {
+    localStorage.removeItem('loggedInUserId');
+    signOut(auth)
+        .then(() => {
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            console.error('Error al cerrar sesión:', error);
+        });
+});
+
+// Manejo del botón de eliminar cuenta
+const deleteAccountButton = document.getElementById('deleteAccount');
+
+deleteAccountButton.addEventListener('click', () => {
+    const confirmDelete = confirm("¿Estás seguro de que deseas eliminar tu cuenta? Esta acción es permanente y no se puede revertir. Pero puedes registrarte de nuevo");
+    if (confirmDelete) {
+        const loggedInUserId = localStorage.getItem('loggedInUserId');
+        const user = auth.currentUser;
+
+        // Eliminar el documento de Firestore
+        const docRef = doc(db, "users", loggedInUserId);
+        deleteDoc(docRef)
+            .then(() => {
+                console.log("Documento eliminado correctamente de Firestore");
+
+                // Eliminar la cuenta de Firebase Authentication
+                deleteUser(user)
+                    .then(() => {
+                        console.log("Cuenta eliminada correctamente de Firebase Authentication");
+                        localStorage.removeItem('loggedInUserId');
+                        window.location.href = 'index.html';
+                    })
+                    .catch((error) => {
+                        console.error("Error al eliminar la cuenta de Firebase Authentication: ", error);
+                    });
+            })
+            .catch((error) => {
+                console.error("Error al eliminar el documento de Firestore: ", error);
+            });
+    }
+});
---
+++ reportes/script/scripchequeo.js
@@ -0,0 +1,124 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.getElementById('signatureModal');
+    const modalCanvas = document.getElementById('modalCanvas');
+    const ctx = modalCanvas.getContext('2d');
+    let isDrawing = false; // Variable para saber si está dibujando
+    let currentCanvas = null; // Canvas actual para firmar
+
+    // Abre el modal al hacer clic en cualquier canvas de firma
+    document.getElementById('reportadoPorTable').addEventListener('click', function (event) {
+        if (event.target.classList.contains('signatureCanvas')) {
+            currentCanvas = event.target; // Guarda la referencia del canvas actual
+            modal.style.display = "block";
+            ctx.clearRect(0, 0, modalCanvas.width, modalCanvas.height); // Limpiar el canvas del modal antes de empezar
+
+            // Ajustar el tamaño del canvas modal para que se adapte al modal
+            const modalContent = document.querySelector('.modal-content');
+            modalCanvas.width = modalContent.clientWidth - 40; // Margen (20px a cada lado)
+            modalCanvas.height = 150; // Altura fija
+        }
+    });
+
+    // Manejo del modal de firma
+    const closeModal = document.querySelector('.close');
+    closeModal.onclick = function () {
+        modal.style.display = "none";
+    };
+
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
+
+    // Manejador para el dibujo en el canvas
+    const startDrawing = (e) => {
+        isDrawing = true;
+        draw(e);
+    };
+
+    const stopDrawing = () => {
+        isDrawing = false;
+        ctx.beginPath(); // Reiniciar el camino
+    };
+
+    const draw = (e) => {
+        if (!isDrawing) return; // Si no está dibujando, salir
+
+        // Obtener la posición del mouse o toque en relación al canvas
+        const rect = modalCanvas.getBoundingClientRect();
+        const x = (e.clientX || e.touches[0].clientX) - rect.left; // Coordenada X ajustada
+        const y = (e.clientY || e.touches[0].clientY) - rect.top; // Coordenada Y ajustada
+
+        ctx.lineWidth = 2; // Espesor de la línea
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'rgb(13, 0, 255)'; // Color negro vivo con opacidad máxima
+
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    };
+
+    // Añadir eventos para mouse y touch
+    modalCanvas.addEventListener('mousedown', startDrawing);
+    modalCanvas.addEventListener('mouseup', stopDrawing);
+    modalCanvas.addEventListener('mousemove', draw);
+    modalCanvas.addEventListener('touchstart', (e) => { e.preventDefault(); startDrawing(e); });
+    modalCanvas.addEventListener('touchend', stopDrawing);
+    modalCanvas.addEventListener('touchmove', (e) => { e.preventDefault(); draw(e); });
+
+    // Guardar la firma en el canvas específico de la tabla
+    document.getElementById('saveSignature').addEventListener('click', function () {
+        if (currentCanvas) {
+            const signatureCtx = currentCanvas.getContext('2d');
+            signatureCtx.drawImage(modalCanvas, 0, 0, currentCanvas.width, currentCanvas.height);
+        }
+        modal.style.display = "none"; // Cerrar el modal
+    });
+
+    // Limpiar las firmas
+    document.getElementById('clearSignatureReportedBy').addEventListener('click', function () {
+        const canvas = document.querySelector('#reportadoPorTable tbody tr td .signatureCanvas');
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar el canvas
+    });
+
+    // Manejador para los botones de limpiar firma y eliminar fila
+    document.getElementById('reportadoPorTable').addEventListener('click', function (event) {
+        // Limpiar firma
+        if (event.target.classList.contains('clearSignatureButton')) {
+            const row = event.target.closest('tr'); // Obtener la fila correspondiente
+            const canvas = row.querySelector('.signatureCanvas'); // Obtener el canvas de esa fila
+            const ctx = canvas.getContext('2d'); // Obtener su contexto
+            ctx.clearRect(0, 0, canvas.width, canvas.height); // Limpiar el canvas
+        }
+
+        // Eliminar fila
+        if (event.target.classList.contains('deleteRowButton')) {
+            const row = event.target.closest('tr'); // Obtener la fila correspondiente
+            row.nextElementSibling.remove(); // Eliminar la fila de observaciones
+            row.remove(); // Eliminar la fila de la tabla
+        }
+    });
+});
+
+// Configuración de la fecha de servicio
+document.addEventListener('DOMContentLoaded', function() {
+    const fechaServicioInput = document.getElementById('fecha-servicio');
+    const today = new Date();
+    const year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let day = today.getDate();
+
+    // Agregar un cero delante del mes y día si son menores a 10
+    if (month < 10) {
+        month = '0' + month;
+    }
+    if (day < 10) {
+        day = '0' + day;
+    }
+
+    const todayFormatted = `${year}-${month}-${day}`;
+    fechaServicioInput.value = todayFormatted;
+});
